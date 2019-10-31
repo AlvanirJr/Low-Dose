b90e233200cc6c23ab6788885d15f7035a927ba9
Andreimd02 <33467988+Andreimd02@users.noreply.github.com>
2019-10-30 21:04:49 -0300

    Add files via upload

--- DataInterface.py
+++ DataInterface.py
@@ -68,11 +68,11 @@ class DataInterface():
         return (training_files, validation_files, testing_files)
 
     def get_axis_coronal(self):
-        global src
-        global src_coronal
+        global src_segmented
+        global src_segmented_coronal
 
         tomografias = []
-        for root, _, files in os.walk(src):
+        for root, _, files in os.walk(src_segmented):
             for z in files:
                 linha = z.split("_")
                 tomografias.append(int(linha[1]))
@@ -80,16 +80,24 @@ class DataInterface():
         for tomo in tomografias:
             volume = self.get_tomo_volume(tomo)
             for j in range(len(volume)):
-                im = volume[:, j, :]
-                misc.imsave(os.path.join(src_coronal, "Tomo_{}_slice_{}.png".format(str(tomo).zfill(3),str(j).zfill(3))), im)
+                im = np.array(volume[:, j, :])
+                m, n = im.shape
+                out = im
+                if(m % 32 != 0):
+                    out = np.zeros((m+ (32 - (m % 32)), n ), dtype=im.dtype)
+                    out[:m, :] = im
+                if(n % 32 != 0):
+                    out = np.zeros((m, n + (32 - (n % 32))), dtype=im.dtype)
+                    out[:, :n] = im
+                misc.imsave(os.path.join(src_segmented_coronal, "Tomo_{}_slice_{}.png".format(str(tomo).zfill(3),str(j).zfill(3))), out)
 
 
     def get_axis_sagittal(self):
-        global src
-        global src_sagittal
+        global src_lowdose
+        global src_lowdose_sagittal
 
         tomografias = []
-        for root, _, files in os.walk(src):
+        for root, _, files in os.walk(src_lowdose):
             for z in files:
                 linha = z.split("_")
                 tomografias.append(int(linha[1]))
@@ -97,9 +105,19 @@ class DataInterface():
         for tomo in tomografias:
             volume = self.get_tomo_volume(tomo)
             for j in range(len(volume)):
-                im = volume[j, :, :]
-                misc.imsave(os.path.join(src_sagittal, "Tomo_{}_slice_{}.png".format(str(tomo).zfill(3), str(j).zfill(3))),
-                          im)
+                im = np.array(volume[j, :, :])
+                m, n = im.shape
+                out = im
+                if (m % 32 != 0):
+                    out = np.zeros((m + (32 - (m % 32)), n), dtype=im.dtype)
+                    out[:m, :] = im
+                if (n % 32 != 0):
+                    out = np.zeros((m, n + (32 - (n % 32))), dtype=im.dtype)
+                    out[:, :n] = im
+
+                misc.imsave(os.path.join(src_lowdose_sagittal, "Tomo_{}_slice_{}.png".format(str(tomo).zfill(3), str(j).zfill(3))),
+                          out)
+
 
 
 if __name__ == "__main__":
@@ -107,9 +125,22 @@ if __name__ == "__main__":
     src = "/home/andrei/Área de Trabalho/Pesquisa/DATASET-256/"
     src_coronal = "/home/andrei/Área de Trabalho/Pesquisa/DATASET-256-CORONAL/"
     src_sagittal = "/home/andrei/Área de Trabalho/Pesquisa/DATASET-256-SAGITTAL/"
+
+    src_segmented = "/home/andrei/Área de Trabalho/Pesquisa/DATASET-256-GT/"
+    src_segmented_coronal = "/home/andrei/Área de Trabalho/Pesquisa/DATASET-256-CORONAL-GT/"
+    src_segmented_sagittal = "/home/andrei/Área de Trabalho/Pesquisa/DATASET-256-SAGITTAL-GT/"
+
+    src_lowdose = "/home/andrei/Área de Trabalho/Pesquisa/DATASET-256 LOW-DOSE/15_projections/"
+    src_lowdose_coronal = "/home/andrei/Área de Trabalho/Pesquisa/DATASET-256-CORONAL LOW-DOSE/15_projections/"
+    src_lowdose_sagittal = "/home/andrei/Área de Trabalho/Pesquisa/DATASET-256-SAGITTAL LOW-DOSE/15_projections/"
+
+    src_lowdose_gt = "/home/andrei/Área de Trabalho/Pesquisa/DATASET-256 LOW-DOSE/15_projections/"
+    src_lowdose_gt_coronal = "/home/andrei/Área de Trabalho/Pesquisa/DATASET-256-CORONAL LOW-DOSE/15_projections/"
+    src_lowdose_gt_sagittal = "/home/andrei/Área de Trabalho/Pesquisa/DATASET-256-SAGITTAL LOW-DOSE/15_projections/"
+
     dataset = DataInterface(src)
     dataset.get_axis_coronal()
-    dataset.get_axis_sagittal()
+    # dataset.get_axis_sagittal()
     #print(dataset.get_tomo_list())
     volume = dataset.get_tomo_volume(0)
     print(volume.shape)
